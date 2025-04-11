953b18f2425db031b3c955848a91cc0bf8237b76
Andreza Gaspari <168014654+AndrezaGaspari@users.noreply.github.com>
2025-04-11 20:51:52 -0300

    Delete sistema-revenda/test_db.py

--- sistema-revenda/test_db.py
+++
@@ -1,8 +0,0 @@
-from database import SessionLocal
-
-try:
-    db = SessionLocal()
-    print("Conexão com SQL Server bem-sucedida!")
-    db.close()
-except Exception as e:
-    print("Erro ao conectar ao banco:", e)
