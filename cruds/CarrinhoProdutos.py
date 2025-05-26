5e28fc09fbff3f1316cbfdbdf78855ffbae3716f
GustavoGroto <groto.gustavo@gmail.com>
2025-05-25 21:37:49 -0300

    Carrinho pronto

--- cruds/CarrinhoProdutos.py
+++ cruds/CarrinhoProdutos.py
@@ -1,14 +1,49 @@
 from sqlalchemy.orm import Session
 from banco_de_dados import models, schemas
 
-def criar_carrinho():
-    return 0
+def adicionar_Produto_carrinho(db: Session, item: schemas.CarrinhoProdutoCreate):
+    db_item = db.query(models.CarrinhoProduto).filter(
+        models.CarrinhoProduto.produto_id == item.produto_id,
+        models.CarrinhoProduto.revendedor_id == item.revendedor_id
+    ).first()
 
-def listar_carrinho():
-    return 0 
+    if db_item:
+        db_item.quantidade += item.quantidade
+    else:
+        db_item = models.CarrinhoProduto(
+            produto_id=item.produto_id,
+            revendedor_id=item.revendedor_id,
+            quantidade=item.quantidade
+        )
+        db.add(db_item)
 
-def buscar_carrinho():
-    return 0
+    db.commit()
+    db.refresh(db_item)
+    return db_item
 
-def atualizar_carrinho():
-    return 0
+
+# Listar itens do models.carrinho por revendedor
+def listar_Produto_carrinho(db: Session, revendedor_id: int):
+    return db.query(models.CarrinhoProduto).filter(models.CarrinhoProduto.revendedor_id == revendedor_id)
+
+
+
+def remover_Produto_carrinho(db: Session, revendedor_id: int, produto_id: int):
+    db_item = db.query(models.CarrinhoProduto).filter(
+        models.CarrinhoProduto.revendedor_id == revendedor_id,
+        models.CarrinhoProduto.produto_id == produto_id
+    ).first()
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+        return True
+    return False
+
+
+# Limpar models.carrinho todo de um revendedor
+def limpar_carrinho(db: Session, revendedor_id: int):
+    itens = db.query(models.CarrinhoProduto).filter(models.CarrinhoProduto.revendedor_id == revendedor_id)
+
+    for Produto in itens:
+        db.delete(Produto)
+    db.commit()
